112a5bd4a0c0bb0702cac998a8e7db9e7c261464
limarkdl <116545670+limarkdl@users.noreply.github.com>
2023-03-08 15:50:09 +0200

    Update from main project

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ let URL = "https://codecyprus.org/th/api";
 let questionType = {"BOOLEAN": "radio", "INTEGER": "number", "TEXT": "text", "NUMERIC": "number", "MCQ": "radio"};
 var geotimer;
 
+let playerName = "Anonim";
+
+// Elements' paths//
+const body = document.body;
+const soundIcon = document.querySelector('#soundIcon');
+const score = document.getElementById('score');
+const dialogue = document.getElementsByClassName('dialogue');
+const questionBox = document.getElementsByClassName('.questionBox');
+//
 let backgroundMusic = true;
 document.addEventListener("DOMContentLoaded", function () {
     var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-    console.log(scrollbar);
+    // console.log(scrollbar);
     document.querySelector('[href="#openModal"]').addEventListener('click', function () {
         document.body.style.overflow = 'hidden';
         document.querySelector('#openModal').style.marginLeft = scrollbar;
@@ -16,30 +25,45 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
-
 function soundVolumeToggle() {
     if (backgroundMusic) {
-        document.getElementById('soundIcon').src = 'soundOFFIcon.png';
+        soundIcon.src = 'soundOFFIcon.png';
         backgroundMusic = false;
     } else {
-        document.getElementById('soundIcon').src = 'soundONIcon.png';
+        soundIcon.src = 'soundONIcon.png';
         backgroundMusic = true;
     }
 
 }
 
 function displayDialogue() {
-    document.getElementsByClassName('dialogue')[0].style.display = 'none';
+    dialogue[0].style.display = 'none';
     document.getElementsByClassName('questionBox')[0].style.opacity = '0%';
     document.getElementsByClassName('mascotBox')[0].style.opacity = '0%';
     document.getElementsByClassName('answerBox')[0].style.opacity = '0%';
-    setTimeout(()=>{document.getElementsByClassName('dialogue')[0].style.display = 'flex';},500)
-    setTimeout(()=>{document.getElementsByClassName('questionBox')[0].style.opacity = '100%';
-        document.getElementsByClassName('mascotBox')[0].style.opacity = '100%';},1000)
-    setTimeout(function (){document.getElementsByClassName('answerBox')[0].style.opacity = '100%';},2000);
+    setTimeout(() => {
+        dialogue[0].style.display = 'flex';
+    }, 100)
+    setTimeout(() => {
+        document.getElementsByClassName('questionBox')[0].style.opacity = '100%';
+        document.getElementsByClassName('mascotBox')[0].style.opacity = '100%';
+    }, 400)
+    setTimeout(function () {
+        document.getElementsByClassName('answerBox')[0].style.opacity = '100%';
+    }, 1000);
+    setTimeout(() => {
+        document.getElementsByClassName('dialogue')[0].style.display = 'flex';
+    }, 500)
+    setTimeout(() => {
+        document.getElementsByClassName('questionBox')[0].style.opacity = '100%';
+        document.getElementsByClassName('mascotBox')[0].style.opacity = '100%';
+    }, 1000)
+    setTimeout(function () {
+        document.getElementsByClassName('answerBox')[0].style.opacity = '100%';
+    }, 2000);
 }
 
+// Toggles Fullscreen mode
 function fullScreenToggle() {
     let el = document.body;
     if (document.fullscreenElement) {
@@ -47,10 +71,7 @@ function fullScreenToggle() {
     } else {
         el.requestFullscreen();
     }
-
 }
-
-
 
 function listHunts() {
     let main = document.getElementById("main");
@@ -61,21 +82,20 @@ function listHunts() {
     main.appendChild(list);
 
     displayDialogue();
-    fetch(URL + "/list").then(response => response.json()).then(json => {
-
+    fetch(URL + "/list").then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
         for (let i = 0; i < json.treasureHunts.length; i++) {
             let entry = document.createElement("li");
             entry.innerHTML = "<button onclick=registerPlayer(\'" + json.treasureHunts[i].uuid + "\')>" + json.treasureHunts[i].name + "</button>";
             list.appendChild(entry);
         }
-
         document.getElementById('answerBox').innerText = "Hmmm, let me see...";
         document.getElementById('answerBox').style.opacity = "1";
+    }).catch(function (error) {
+        networkErrors(error);
     })
-
 }
 
-function showLeaderboard(){
+function showLeaderboard() {
     //Clean page
     let main = document.getElementById("main");
     main.innerHTML = "";
@@ -89,7 +109,7 @@ function showLeaderboard(){
 
     let session = localStorage.getItem("session");
     displayDialogue();
-    fetch(URL + "/leaderboard?session=" + session + "&sorted").then(response => response.json()).then(json => {
+    fetch(URL + "/leaderboard?session=" + session + "&sorted").then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
 
         for (let i = 0; i < json.numOfPlayers; i++) {
             let entry = document.createElement("li");
@@ -98,6 +118,8 @@ function showLeaderboard(){
         }
 
         document.getElementById('answerBox').innerText = 'Oh...';
+    }).catch(function (error) {
+        networkErrors(error);
     })
 
 
@@ -105,6 +127,7 @@ function showLeaderboard(){
 
 function registerPlayer(huntID) {
     //Clean page
+    displayDialogue();
     document.getElementById("main").innerHTML = "";
     document.getElementById("main").innerHTML = "<p>What is your name?</p>";
     //Starting geolocation counter
@@ -119,17 +142,24 @@ function registerPlayer(huntID) {
 }
 
 function startHunt(huntID) {
-
-    let playerName = document.getElementById("playerInput").value;
-    //Clean page
-    document.getElementById("main").innerHTML = "";
-
-    fetch(URL + "/start?player=" + playerName + "&app=ourApp&treasure-hunt-id=" + huntID).then(response => response.json()).then(json => {
-        console.log(json);
-        //store session
-        localStorage.setItem("session", json.session);
-        displayCurrentQuestion();
+    let playerNameInput = document.getElementById("playerInput").value;
+    fetch(URL + "/start?player=" + playerNameInput + "&app=ourApp&treasure-hunt-id=" + huntID).then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
+        // check json
+        if (checkIsNameOK(json) === 1) {
+            //store session if there is no errors
+            localStorage.setItem("session", json.session);
+            playerName = playerNameInput;
+            document.getElementById('playersName').innerText = playerName + ':';
+            document.getElementById("main").innerHTML = "";
+            displayCurrentQuestion();
+            createListOfChests(json.numOfQuestions);
+        } else {
+            registerPlayer(huntID);
+        }
+    }).catch(function (error) {
+        networkErrors(error);
     })
+
 
 }
 
@@ -140,10 +170,8 @@ function displayCurrentQuestion() {
     getScore();
     displayDialogue();
     //Get question from server
-    fetch(URL + "/question?session=" + session).then(response => response.json()).then(json => {
-        console.log(json);
-
-        if(json.completed){
+    fetch(URL + "/question?session=" + session).then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
+        if (json.completed) {
             showLeaderboard();
             return;
         }
@@ -156,34 +184,36 @@ function displayCurrentQuestion() {
 
         displayInputHTML(json, "answerQuestion");
 
+    }).catch(function (error) {
+        networkErrors(error);
     })
 }
 
-function getScore(){
+function getScore() {
     let session = localStorage.getItem("session");
 
-    fetch(URL + "/score?session=" + session).then(response => response.json()).then(json => {
-        console.log(json);
+    fetch(URL + "/score?session=" + session).then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
         document.getElementById("score").innerHTML = json.score;
+    }).catch(function (error) {
+        networkErrors(error);
     })
 }
 
-function skipQuestion(){
+function skipQuestion() {
     let session = localStorage.getItem("session");
 
-    fetch(URL + "/skip?session=" + session).then(response => response.json()).then(json => {
-        console.log(json);
+    fetch(URL + "/skip?session=" + session).then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
         displayCurrentQuestion();
+    }).catch(function (error) {
+        networkErrors(error);
     })
 }
 
 function answerQuestion() {
-
-    document.getElementById("submitButton").setAttribute("disabled", "");
-
+    //document.getElementById("submitButton").setAttribute("disabled", ""); TODO: why do we need this line?
     let session = localStorage.getItem("session");
     let playerAnswer;
-    if(document.getElementById("playerInput") !== null){
+    if (document.getElementById("playerInput") !== null) {
         playerAnswer = document.getElementById("playerInput").value;
     } else {
         //Getting the bool or MCQ value from a radio button
@@ -196,20 +226,20 @@ function answerQuestion() {
             }
         }
     }
-    fetch(URL + "/answer?session=" + session + "&answer=" + playerAnswer).then(response => response.json()).then(json => {
-        console.log(json);
+    fetch(URL + "/answer?session=" + session + "&answer=" + playerAnswer).then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
         if (json.correct) {
             displayCurrentQuestion();
         } else {
             alert("You have answered incorrectly!");
-            displayCurrentQuestion();
         }
+    }).catch(function (error) {
+        networkErrors(error);
     })
 }
 
-function getLocation(){
+// Gets current user's location
+function getLocation() {
     if (navigator.geolocation) {
-
         navigator.geolocation.getCurrentPosition(coordinates => {
             let latitude = coordinates.coords.latitude;
             let longitude = coordinates.coords.longitude;
@@ -217,51 +247,34 @@ function getLocation(){
             sendLocation(latitude, longitude);
 
         });
-    }
-    else {
+    } else {
         //TODO - Geolocation is NOT supported by browser.
         alert("Geolocation is not supported by your browser.");
 
     }
 }
 
-function sendLocation(latitude, longitude){
 
-
+function sendLocation(latitude, longitude) {
     let session = localStorage.getItem("session");
-
-    fetch(URL + "/location?session=" + session + "&latitude=" + latitude + "&longitude=" + longitude).then(response => response.json()).then(json => {
-        console.log(json);
-        //TODO - handle errors
-        if(json.status !== "OK"){
-
-        }
+    fetch(URL + "/location?session=" + session + "&latitude=" + latitude + "&longitude=" + longitude).then(response => response.ok ? response.json() : Promise.reject(response)).then(json => {
+    }).catch(function (error) {
+        networkErrors(error);
     })
 
 }
 
 function displayInputHTML(inputObject, functionCall, functionParam = "") {
 
-
     let form = document.createElement("form");
     form.setAttribute("onsubmit", functionCall + "(\'" + functionParam + "\'); return false;");
     let answerBox = document.getElementById('answerBox');
     answerBox.innerHTML = "";
     if (inputObject.questionType === 'BOOLEAN') {
-        form.innerHTML = "<input required type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"true\">\n" +
-                         "<label for=\"playerInput\">True</label>\n" +
-                         "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"false\">\n" +
-                         "<label for=\"playerInput\">False</label><br>"
+        form.innerHTML = "<input required type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"true\">\n" + "<label for=\"playerInput\">True</label>\n" + "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"false\">\n" + "<label for=\"playerInput\">False</label><br>"
 
     } else if (inputObject.questionType === 'MCQ') {
-        form.innerHTML = "<input required type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"A\">\n" +
-                         "<label for=\"playerInput\">A</label>\n" +
-                         "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"B\">\n" +
-                         "<label for=\"playerInput\">B</label>\n" +
-                         "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"C\">\n" +
-                         "<label for=\"playerInput\">C</label>\n" +
-                         "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"D\">\n" +
-                         "<label for=\"playerInput\">D</label><br>"
+        form.innerHTML = "<input required type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"A\">\n" + "<label for=\"playerInput\">A</label>\n" + "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"B\">\n" + "<label for=\"playerInput\">B</label>\n" + "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"C\">\n" + "<label for=\"playerInput\">C</label>\n" + "<input type=\"radio\" class=\"playerInput\" name=\"playerInput\" value=\"D\">\n" + "<label for=\"playerInput\">D</label><br>"
 
     } else {
         let playerAnswerField = document.createElement("input");
@@ -274,8 +287,8 @@ function displayInputHTML(inputObject, functionCall, functionParam = "") {
         form.appendChild(playerAnswerField);
     }
 
-    // Skip buttion
-    if(inputObject.canBeSkipped) {
+    // Skip button
+    if (inputObject.canBeSkipped) {
         let skipButton = document.createElement("button");
         skipButton.innerText = "SKIP";
         skipButton.setAttribute("onclick", "letAnimationComplete(skipQuestion())");
@@ -288,5 +301,47 @@ function displayInputHTML(inputObject, functionCall, functionParam = "") {
     document.getElementById("main").appendChild(form);
     answerBox.appendChild(form);
     form.appendChild(submit);
+
     document.getElementById("answerBox").style.opacity = "1";
+
+}
+
+// functions for managing errors
+const playerNameInUseErrorMessage = "The specified playerName: , is already in use (try a different one).",
+    missingParameterErrorMessage = "Missing or empty parameter: app",
+    unknownTreasureHuntIdErrorMessage = "Could not find a treasure hunt for the specified id:";
+
+function checkIsNameOK(json) {
+    if ("OK" !== json.status) {
+        console.log(json);
+        for (let i = 0; i < json.errorMessages.length; ++i) {
+            console.log(json.errorMessages[i]);
+            if (playerNameInUseErrorMessage[0] === json.errorMessages[i][0]) {
+                alert(json.errorMessages[i]);
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+function networkErrors(error) {
+    console.log(error);
+    alert("Ooops... Some problems with connection");
+}
+
+// This function clears the local session to be able to play/test again
+const clearSessionBt = () => {
+    localStorage.clear();
+    alert('Cleared!')
+};
+
+// This function creates list of chests to display progress of the user
+function createListOfChests(numOfQuestions) {
+    document.querySelector('.progressBar').innerHTML = '';
+    for (let i = 0; i < numOfQuestions; i++) {
+        let chest = document.createElement('img');
+        chest.src = './media/chestClosed_LowRes.png';
+        document.querySelector('.progressBar').appendChild(chest);
+    }
 }
--- notes.md
+++ notes.md
@@ -1,7 +1,7 @@
 # Minutes for the SeMaKo Group
 
 ## Session 1  
-Date:30.01.2023  
+Date: 30.01.2023  
 Start: 12:00  
 End:  13:00  
 Location: Cyprus, Pyla, Uclan Campus, CY133  
@@ -10,16 +10,10 @@ Participants: Serdyukov, Marchenko, Kostin
 Plan initiated. Preliminary roles distributed for this week.  
 Ivan - UI/UX development.  
 Antoniy - functionality.  
-Ksenia - Social Media Marketing/PR
-
-
-
-
-
-
+Kseniia - Social Media Marketing/PR
 
 ## Session 2  
-Date:03.02.2023  
+Date: 03.02.2023  
 Start: 11:00  
 End: 12:00  
 Location: Cyprus, Pyla, Uclan Campus, CY014  
@@ -27,4 +21,16 @@ Participants: Serdyukov, Marchenko, Kostin
 
 Concept art is defined. Question displyament is showcased.  
 The art and font is decided to be pixelated. 
-Twitter intergration is for Ksenia.
+Twitter intergration is for Kseniia.
+
+## Session 3  
+Date: 03.03.2023  
+Start: 10:00  
+End: 12:00  
+Location: Cyprus, Pyla, Uclan Campus, CY014  
+Participants: Serdyukov, Marchenko, Kostin  
+
+Kseniia works on errors, server specific errors are displayed for the user throught UI; other errors lead to 404.
+Ivan works on CSS, fixes design bugs. 
+Antoniy work on test.html, more specifically unit-testing.
+Team creates a plan of testing.
